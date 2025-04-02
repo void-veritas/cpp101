f9997ead8254ad5f00c2ab95b807310cc4531a8c
eprokhodtsev <e.prokhodtsev@gmail.com>
2025-04-02 23:09:51 +0100

    Working with randoms and conditionals

--- chapter1/1_1 variables/main.cpp
+++ chapter1/1_1 variables/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <random>
 //#include <print>
 //import std;
 //import <print>;
@@ -46,6 +47,37 @@ int main() {
     println("Some Double: {}",someDouble);
 
     println("Trying multiplying short by 1_000_000_000 {}",someShort*1000000000);
+
+//messing around with enumerators
+    enum class PieceType
+    {
+        King = 9,
+        Queen,
+        Rook = 10,
+        Pawn
+    };
+    PieceType piece {PieceType::King};
+    int underlyingValue {to_underlying(piece)};
+    println ("enum underlying {}", underlyingValue);
+
+    PieceType piece2 {PieceType::Rook};
+    int underlyingValue2 {to_underlying(piece2)};
+    println ("enum underlying2 {}", underlyingValue);
+
+
+    // Create a random number generator
+    std::random_device rd;                         // Seed
+    std::mt19937 gen(rd());                        // Mersenne Twister engine
+    std::uniform_int_distribution<> distr(1, 100); // Range: [1, 100]
+
+    //conditional statements
+    int random_num1 = distr(gen);;
+    int random_num2 = distr(gen);;
+    if (random_num1>random_num2) {
+        println("{} is greater than {}",random_num1,random_num2);
+    }else {
+        println("{} is less than {}",random_num1,random_num2);
+    }
 
     return 0;
 }
